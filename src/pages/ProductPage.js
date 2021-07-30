a97b499c7b586265232a32702ac428355c12e37b
jaekyung lee <jaekyunglee@jaekyungui-MacBookPro.local>
2021-07-30 17:01:55 +0900

    Feat: UI 완성

--- src/pages/ProductPage.js
+++ src/pages/ProductPage.js
@@ -1,12 +1,13 @@
 import React, { Component } from "react";
-import { ERROR_MESSAGE } from "utils/config";
 import { v4 as uuidv4 } from "uuid";
+import { ERROR_MESSAGE } from "utils/config";
+import "./Product.css";
 
 class ProductPage extends Component {
 	constructor() {
 		super();
 		this.state = {
-			data: [],
+			products: [],
 		};
 	}
 
@@ -14,9 +15,13 @@ class ProductPage extends Component {
 		try {
 			const res = await fetch("http://localhost:3005/data/productData.json");
 			const data = await res.json();
-			this.setState({ data: data });
-			this.state.data.map((x) => (x.id = uuidv4()));
-			console.log(this.state.data);
+			this.setState({
+				products: data.map((product) => {
+					product.id = uuidv4();
+					return product;
+				}),
+			});
+			console.log(this.state.products);
 			if (!res.ok) {
 				throw new Error(ERROR_MESSAGE);
 			}
@@ -24,11 +29,30 @@ class ProductPage extends Component {
 			console.error(err);
 		}
 	}
+	removeProduct(id) {
+		this.setState({
+			products: this.state.products.filter((product) => product.id !== id),
+		});
+		console.log("work!");
+	}
 
 	render() {
 		return (
-			<div>
-				<p></p>
+			<div className="container">
+				<button className="random">랜덤상품조회</button>
+				<ul className="products">
+					{this.state.products.map((product) => {
+						const { title, id, price, brand } = product;
+						return (
+							<li className="product" key={id}>
+								<h2 className="title">{title}</h2>
+								<h4 className="brand">{brand}</h4>
+								<p className="price">{price}</p>
+								<button onClick={() => this.removeProduct(id)}>관신없음</button>
+							</li>
+						);
+					})}
+				</ul>
 			</div>
 		);
 	}
