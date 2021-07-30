7e3f050c1ce91d5794df7d99b038866f9fa22038
dongwon <kkum9408@gmail.com>
2021-07-30 17:30:58 +0900

    Feat: 숨기기 체크 박스 구현
    - 체크 박스 선택 시 onInterset 함수에서 !interest일 때 filter 메소드 이용

--- src/pages/RecentListPage.js
+++ src/pages/RecentListPage.js
@@ -1,10 +1,50 @@
 import React, { Component } from "react";
 
 class RecentListPage extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      recentData: JSON.parse(localStorage.getItem("data")),
+      tempData: [],
+    };
+  }
+
+  onInterset(checked) {
+    const { recentData, tempData } = this.state;
+    // checked === true면 관심 없는 체크박스를 체크한 것
+    if (checked) {
+      this.setState({
+        tempData: [...recentData],
+        recentData: recentData.filter((data) => !data.interest),
+      });
+    } else {
+      this.setState({
+        recentData: [...tempData],
+      });
+    }
+  }
+
   render() {
+    const { recentData } = this.state;
     return (
       <div>
         <p>RecentListPage</p>
+        {recentData.map((data) => (
+          <div key={data.id}>
+            <p>id : {data.id}</p>
+            <p> title : {data.title}</p>
+            <p> brand : {data.brand}</p>
+            <p> price : {data.price}</p>
+            <p> interest : {data.interest}</p>
+          </div>
+        ))}
+        <label htmlFor="check interest">관심 없는 상품 제거하기</label>
+        <input
+          type="checkbox"
+          id="check interest"
+          onChange={(e) => this.onInterset(e.target.checked)}
+        />
       </div>
     );
   }
