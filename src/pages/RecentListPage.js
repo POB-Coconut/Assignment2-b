3b1929c822cb8d20d98af45f6b237baae110d373
dongwon <kkum9408@gmail.com>
2021-07-30 12:25:55 +0900

    Feat: 초기 설정 세팅
    - css 초기화
    - 절대 경로 설정
    - productData.json 설정
    - page 별 router 설정

---
+++ src/pages/RecentListPage.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class RecentListPage extends Component {
+  render() {
+    return (
+      <div>
+        <p>RecentListPage</p>
+      </div>
+    );
+  }
+}
+
+export default RecentListPage;
